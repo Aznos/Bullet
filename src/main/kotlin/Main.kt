073f87878e166756d44510f9487d79389bda0a8d
aznos <pigiotyreal@gmail.com>
2025-02-08 19:53:09 -0500

    Linking it all together

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,32 +6,5 @@ import java.nio.charset.Charset
 import kotlin.math.exp
 
 fun main() {
-    val varIntUtil = VarInt()
-
-    //VAR INT TEST
-    val expectedVarInt = 300
-    val varIntBytes = byteArrayOf(0xAC.toByte(), 0x02.toByte())
-    val varIntInput = ByteArrayInputStream(varIntBytes)
-    val readVarInt = with(varIntUtil) { varIntInput.readVarInt() }
-
-    println("Expected VarInt: $expectedVarInt, Read VarInt: $readVarInt")
-    if(readVarInt != expectedVarInt) {
-        println("VarInt test failed")
-    } else {
-        println("VarInt test passed")
-    }
-
-    //READ STRING TEST
-    val expectedString = "Hello"
-    val stringBytes = expectedString.toByteArray(Charsets.UTF_8)
-    val encodedString = byteArrayOf(0x05) + stringBytes
-    val stringInput = ByteArrayInputStream(encodedString)
-    val readString = with(varIntUtil) { stringInput.readString() }
-
-    println("Expected String: $expectedString, Read String: $readString")
-    if(readString != expectedString) {
-        println("Read string test failed")
-    } else {
-        println("Read string test passed")
-    }
+    Server.start()
 }
