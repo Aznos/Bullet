023e31923296b36343bf3ef454eb4e2ab27e4920
aznos <pigiotyreal@gmail.com>
2025-02-15 14:40:49 -0500

    Open server

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("io.ktor:ktor-network:2.3.7")
 }
 
 application {
--- src/main/kotlin/Bullet.kt
+++ src/main/kotlin/Bullet.kt
@@ -1,10 +1,25 @@
 package com.aznos
 
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.*
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-fun main() {
+fun main(): Unit = runBlocking(SupervisorJob()) {
     val logger: Logger = LogManager.getLogger("Bullet")
 
-    logger.info("Hello world!")
+    System.setProperty(DEBUG_PROPERTY_NAME, DEBUG_PROPERTY_VALUE_ON)
+
+    val server = aSocket(ActorSelectorManager(Dispatchers.IO))
+        .tcp()
+        .bind("0.0.0.0", 25565)
+
+    while(true) {
+        val socket = server.accept()
+
+        launch(CoroutineName("session/${socket.remoteAddress}")) {
+            logger.info("Accepted connection from ${socket.remoteAddress}")
+        }
+    }
 }
